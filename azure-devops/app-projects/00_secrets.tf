cacde37dc563cecd2b1929923bfa4558c12f0196
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2022-03-15 15:00:21 +0100

    fix: better file names

--- azure-devops/app-projects/00_secrets.tf
+++ azure-devops/app-projects/00_secrets.tf
@@ -31,3 +31,35 @@ module "secrets" {
 #     "aks-azure-devops-sa-token",
 #   ]
 # }
+
+# module "secrets_dev" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.5"
+#   providers = {
+#     azurerm = azurerm.dev
+#   }
+
+#   resource_group = local.dev_key_vault_resource_group
+#   key_vault_name = local.dev_key_vault_name
+
+#   secrets = [
+#     "aks-apiserver-url",
+#     "aks-azure-devops-sa-cacrt",
+#     "aks-azure-devops-sa-token",
+#   ]
+# }
+
+# module "secrets_uat" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v2.0.5"
+#   providers = {
+#     azurerm = azurerm.uat
+#   }
+
+#   resource_group = local.uat_key_vault_resource_group
+#   key_vault_name = local.uat_key_vault_name
+
+#   secrets = [
+#     "aks-apiserver-url",
+#     "aks-azure-devops-sa-cacrt",
+#     "aks-azure-devops-sa-token",
+#   ]
+# }
